e67406705af9f0f5327c92864576453e713ea8ce
xfase <xfaseex1@gmail.com>
2021-02-09 19:18:50 +0300

    feat: create connection

---
+++ src/main/java/com/epam/jwd_final/tiger_bet/connection/ConnectionPool.java
@@ -0,0 +1,137 @@
+package com.epam.jwd_final.tiger_bet.connection;
+
+
+import java.sql.Connection;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public final class ConnectionPool {
+
+    private static final ConnectionPoolProperties connectionPoolProperties =
+            ApplicationContext.getConnectionPoolProperties();
+
+    static final int INITIAL_POOL_SIZE = connectionPoolProperties.getInitialConnections();
+    static final int MAX_POOL_SIZE = connectionPoolProperties.getMaxConnections();
+    static final int EXTRA_CONNECTIONS_AMOUNT = connectionPoolProperties.getExtraConnections();
+    static final double LOAD_FACTOR = connectionPoolProperties.getLoadFactor();
+    static final double SHRINK_FACTOR = connectionPoolProperties.getShrinkFactor();
+
+    private static final Lock INSTANCE_LOCK = new ReentrantLock();
+    private static final Lock CONNECTIONS_LOCK = new ReentrantLock();
+    private static final Condition NOT_FULL = CONNECTIONS_LOCK.newCondition();
+    private static final Condition NOT_EMPTY = CONNECTIONS_LOCK.newCondition();
+
+    private static final AtomicBoolean initialized = new AtomicBoolean(false);
+    private static final AtomicBoolean expandable = new AtomicBoolean(false);
+    private static final AtomicBoolean shrinkable = new AtomicBoolean(false);
+    private static int counter = 0;
+
+    private final Deque<ProxyConnection> availableConnections;
+    private final Deque<ProxyConnection> unavailableConnections;
+
+    private ConnectionPool() {
+        availableConnections = new ArrayDeque<>();
+        unavailableConnections = new ArrayDeque<>();
+    }
+
+    private static class ConnectionPoolHolder {
+        private static final ConnectionPool instance = new ConnectionPool();
+    }
+
+    public static ConnectionPool getInstance() {
+        INSTANCE_LOCK.lock();
+        if (!initialized.get()) {
+            try {
+                if (!initialized.get()) {
+                    ConnectionPoolHolder.instance.init();
+                }
+            } finally {
+                INSTANCE_LOCK.unlock();
+            }
+        }
+        return ConnectionPoolHolder.instance;
+    }
+
+    public Connection retrieveConnection() {
+        CONNECTIONS_LOCK.lock();
+        try {
+            expandable.set(ConnectionPoolManager.INSTANCE.isExpandable());
+            if (expandable.get()) {
+                availableConnections.addAll(ConnectionPoolManager.INSTANCE.expandPool());
+            }
+            while (counter == getAllConnectionsAmount()) {
+                NOT_FULL.await();
+            }
+            ++counter;
+
+            ProxyConnection connection = availableConnections.pollFirst();
+            unavailableConnections.add(connection);
+            NOT_EMPTY.signal();
+            return connection;
+        } catch (InterruptedException e) {
+            // TODO throw new ConnectionPoolException(e);
+            throw new IllegalArgumentException();
+        } finally {
+            CONNECTIONS_LOCK.unlock();
+        }
+    }
+
+    public void returnConnection(Connection connection) {
+        CONNECTIONS_LOCK.lock();
+        try {
+            while (counter == 0) {
+                NOT_EMPTY.await();
+            }
+            if (connection == null || !unavailableConnections.contains((ProxyConnection) connection)) {
+                // TODO throw new ConnectionPoolException(e); // log its better
+                throw new IllegalArgumentException();
+            }
+            boolean result = availableConnections.add((ProxyConnection) connection) &&
+                    unavailableConnections.remove(connection);
+            if (result) {
+                NOT_FULL.signal();
+            }
+        } catch (InterruptedException e) {
+            // TODO throw new ConnectionPoolException(e);
+            e.printStackTrace();
+        } finally {
+            CONNECTIONS_LOCK.unlock();
+        }
+    }
+
+    private void init() {
+        availableConnections.addAll(ConnectionPoolManager.INSTANCE.createConnections(INITIAL_POOL_SIZE));
+        initialized.set(true);
+        ConnectionPoolManager.INSTANCE.createListener();
+    }
+
+    public void destroy() {
+        availableConnections.forEach(ProxyConnection::closeConnection);
+        unavailableConnections.forEach(ProxyConnection::closeConnection);
+    }
+
+    Deque<ProxyConnection> getAvailableConnections() {
+        return availableConnections;
+    }
+
+    AtomicBoolean getIsShrinkable() {
+        return shrinkable;
+    }
+
+    int getAllConnectionsAmount() {
+        return availableConnections.size() + unavailableConnections.size();
+    }
+
+    int getUnavailableConnectionsAmount() {
+        return unavailableConnections.size();
+    }
+
+    int getAvailableConnectionsAmount() {
+        return availableConnections.size();
+    }
+}
+
---
+++ src/main/java/com/epam/jwd_final/tiger_bet/connection/ConnectionPoolManager.java
@@ -0,0 +1,76 @@
+package com.epam.jwd_final.tiger_bet.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public enum ConnectionPoolManager {
+
+    INSTANCE;
+
+    private static final DatabaseProperties databaseProperties =
+            PropertyLoader.getInstance().loadDatabaseProperties();
+
+    void createListener() {
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                ConnectionPool.getInstance().getIsShrinkable().set(ConnectionPoolManager.INSTANCE.isShrinkable());
+                if (ConnectionPool.getInstance().getIsShrinkable().get()) {
+                    ConnectionPoolManager.INSTANCE.shrinkPool();
+                }
+            }
+        };
+        timer.schedule(timerTask, 10_000, 10_000); // TODO read from properties
+    }
+
+    Deque<ProxyConnection> createConnections(int extraConnectionsAmount) {
+        Deque<ProxyConnection> newConnections = new ArrayDeque<>(extraConnectionsAmount);
+        for (int i = 0; i < extraConnectionsAmount; i++) {
+            try {
+                Connection connection = DriverManager.getConnection(
+                        databaseProperties.getUrl(),
+                        databaseProperties.getUser(),
+                        databaseProperties.getPassword());
+                newConnections.add(new ProxyConnection(connection));
+            } catch (SQLException e) {
+                e.printStackTrace(); // todo: log
+            }
+        }
+        return newConnections;
+    }
+
+    boolean isExpandable() {
+        return ConnectionPool.getInstance().getAllConnectionsAmount() * LOAD_FACTOR ==
+                ConnectionPool.getInstance().getUnavailableConnectionsAmount()
+                && ConnectionPool.getInstance().getAllConnectionsAmount() < MAX_POOL_SIZE;
+    }
+
+    Deque<ProxyConnection> expandPool() {
+        return createConnections(EXTRA_CONNECTIONS_AMOUNT);
+        // todo: log
+    }
+
+    boolean isShrinkable() {
+        return ConnectionPool.getInstance().getAllConnectionsAmount() * SHRINK_FACTOR <=
+                ConnectionPool.getInstance().getAvailableConnectionsAmount()
+                && INITIAL_POOL_SIZE < ConnectionPool.getInstance().getAllConnectionsAmount();
+    }
+
+    void shrinkPool() {
+        // todo: log
+        int shrinkSize = (int) Math.min(EXTRA_CONNECTIONS_AMOUNT, ConnectionPool.getInstance().getAllConnectionsAmount() * SHRINK_FACTOR);
+        for (int i = 0; i < shrinkSize && ConnectionPool.getInstance().getAllConnectionsAmount() > INITIAL_POOL_SIZE; i++) {
+            ProxyConnection proxyConnection = ConnectionPool.getInstance().getAvailableConnections().pollFirst();
+            if (proxyConnection != null) {
+                proxyConnection.closeConnection();
+                // todo: log
+            }
+        }
+    }
+}
