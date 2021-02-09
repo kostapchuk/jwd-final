dc43702ebbb2d7db4dac09f8ff1339cedc29f0bb
xfase <xfaseex1@gmail.com>
2021-02-09 20:41:24 +0300

    feat: add connection pool exception

---
+++ src/main/java/com/epam/jwd_final/tiger_bet/exception/ConnectionPoolException.java
@@ -0,0 +1,8 @@
+package com.epam.jwd_final.tiger_bet.exception;
+
+public class ConnectionPoolException extends RuntimeException {
+
+    public ConnectionPoolException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
