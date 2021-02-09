8983e34332b73769785aa7195a72d039395e9444
xfase <xfaseex1@gmail.com>
2021-02-09 19:53:42 +0300

    refactor: remove magic strings

--- src/main/java/com/epam/jwd_final/tiger_bet/util/PropertyLoader.java
+++ src/main/java/com/epam/jwd_final/tiger_bet/util/PropertyLoader.java
@@ -12,39 +12,49 @@ public class PropertyLoader {
 
     private static final Properties properties = new Properties();
 
+    private static final String DATABASE_PROPERTY_PATH = "src/main/resources/properties/database.properties";
+    private static final String CONNECTION_POOL_PROPERTY_PATH = "src/main/resources/properties/connection_pool.properties";
+
+    private static final String DB_URL_PROPERTY = "url";
+    private static final String DB_USER_PROPERTY = "user";
+    private static final String DB_PASSWORD_PROPERTY = "password";
+
+    private static final String POOL_MAX_SIZE_PROPERTY = "maxPoolSize";
+    private static final String POOL_INITIAL_SIZE_PROPERTY = "initialPoolSize";
+    private static final String POOL_EXTRA_CONNECTIONS_AMOUNT_PROPERTY = "extraConnectionAmount";
+    private static final String POOL_LOAD_FACTOR_PROPERTY = "loadFactor";
+    private static final String POOL_SHRINK_FACTOR_PROPERTY = "shrinkFactor";
+    private static final String POOL_CONNECTION_TIME_OUT_PROPERTY = "connectionTimeOut";
+
     private PropertyLoader() {
     }
 
     public DatabaseProperties loadDatabaseProperties() {
-        final String propertiesFileName = "src/main/resources/database.properties"; // TODO
-
-        try (InputStream inputStream = new FileInputStream(propertiesFileName)) {
+        try (InputStream inputStream = new FileInputStream(DATABASE_PROPERTY_PATH)) {
             properties.load(inputStream);
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace(); // TODO log
         }
         return new DatabaseProperties(
-                properties.getProperty("url"),
-                properties.getProperty("user"),
-                properties.getProperty("password")
+                properties.getProperty(DB_URL_PROPERTY),
+                properties.getProperty(DB_USER_PROPERTY),
+                properties.getProperty(DB_PASSWORD_PROPERTY)
         );
     }
 
     public ConnectionPoolProperties loadConnectionPoolProperties() {
-        final String propertiesFileName = "src/main/resources/connection_pool.properties"; // TODO
-
-        try (InputStream inputStream = new FileInputStream(propertiesFileName)) {
+        try (InputStream inputStream = new FileInputStream(CONNECTION_POOL_PROPERTY_PATH)) {
             properties.load(inputStream);
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace(); // TODO log
         }
         return new ConnectionPoolProperties(
-                Integer.parseInt(properties.getProperty("maxPoolSize")),
-                Integer.parseInt(properties.getProperty("initialPoolSize")),
-                Integer.parseInt(properties.getProperty("extraConnectionAmount")),
-                Double.parseDouble(properties.getProperty("loadFactor")),
-                Double.parseDouble(properties.getProperty("shrinkFactor")),
-                Integer.parseInt(properties.getProperty("connectionTimeOut"))
+                Integer.parseInt(properties.getProperty(POOL_MAX_SIZE_PROPERTY)),
+                Integer.parseInt(properties.getProperty(POOL_INITIAL_SIZE_PROPERTY)),
+                Integer.parseInt(properties.getProperty(POOL_EXTRA_CONNECTIONS_AMOUNT_PROPERTY)),
+                Double.parseDouble(properties.getProperty(POOL_LOAD_FACTOR_PROPERTY)),
+                Double.parseDouble(properties.getProperty(POOL_SHRINK_FACTOR_PROPERTY)),
+                Integer.parseInt(properties.getProperty(POOL_CONNECTION_TIME_OUT_PROPERTY))
         );
     }
 
