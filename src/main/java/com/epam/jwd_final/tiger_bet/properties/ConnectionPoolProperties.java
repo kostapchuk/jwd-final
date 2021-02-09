096ef713dee497de0412bb7f117a0eb1fbb8c7eb
xfase <xfaseex1@gmail.com>
2021-02-09 19:24:03 +0300

    feat: add pool properties model

---
+++ src/main/java/com/epam/jwd_final/tiger_bet/properties/ConnectionPoolProperties.java
@@ -0,0 +1,49 @@
+package com.epam.jwd_final.tiger_bet.properties;
+
+public class ConnectionPoolProperties {
+
+    private final int maxConnections;
+    private final int initialConnections;
+    private final int extraConnections;
+    private final double loadFactor;
+    private final double shrinkFactor;
+    private final int connectionTimeOut;
+
+    public ConnectionPoolProperties(int maxConnections,
+                                    int initialConnections,
+                                    int extraConnections,
+                                    double loadFactor,
+                                    double shrinkFactor,
+                                    int connectionTimeOut) {
+        this.maxConnections = maxConnections;
+        this.initialConnections = initialConnections;
+        this.extraConnections = extraConnections;
+        this.loadFactor = loadFactor;
+        this.shrinkFactor = shrinkFactor;
+        this.connectionTimeOut = connectionTimeOut;
+    }
+
+    public int getMaxConnections() {
+        return maxConnections;
+    }
+
+    public int getInitialConnections() {
+        return initialConnections;
+    }
+
+    public int getExtraConnections() {
+        return extraConnections;
+    }
+
+    public double getLoadFactor() {
+        return loadFactor;
+    }
+
+    public double getShrinkFactor() {
+        return shrinkFactor;
+    }
+
+    public int getConnectionTimeOut() {
+        return connectionTimeOut;
+    }
+}
