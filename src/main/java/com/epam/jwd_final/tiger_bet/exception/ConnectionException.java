334f7cc81b941ff865fc35c37ea10b34827d8668
xfase <xfaseex1@gmail.com>
2021-02-09 20:51:48 +0300

    feat: add connection exception

---
+++ src/main/java/com/epam/jwd_final/tiger_bet/exception/ConnectionException.java
@@ -0,0 +1,8 @@
+package com.epam.jwd_final.tiger_bet.exception;
+
+public class ConnectionException extends RuntimeException {
+
+    public ConnectionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
