f5785931cf4128941ac80c75c99062b60d12100c
xfase <xfaseex1@gmail.com>
2021-02-11 15:55:54 +0300

    refactor: implement 2 ,ethods

--- src/main/java/com/epam/jwd_final/tiger_bet/dao/MatchDAO.java
+++ src/main/java/com/epam/jwd_final/tiger_bet/dao/MatchDAO.java
@@ -1,29 +1,34 @@
 package com.epam.jwd_final.tiger_bet.dao;
 
 import com.epam.jwd_final.tiger_bet.domain.Match;
-import com.epam.jwd_final.tiger_bet.domain.Result;
 import com.epam.jwd_final.tiger_bet.mapper.Mapper;
 import com.epam.jwd_final.tiger_bet.mapper.impl.MatchMapper;
 
-import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 public class MatchDAO extends AbstractDAO<Match> {
 
-    public Result retrieveSportTypeById(int id) {
-        return null;
+    private static final String RETRIEVE_MATCH_BY_ID_QUERY = "select * from match where id = ?";
+    private static final String RETRIEVE_MATCHES_BY_SPORT_TYPE_ID_QUERY = "select * from match where sport_type_id = ?";
+
+    public Optional<Match> retrieveMatchById(int id) {
+        return queryForSingleResult(RETRIEVE_MATCH_BY_ID_QUERY, Collections.singletonList(id));
     }
 
-    public String retrieveFirstTeamById(int id) {
-        return null;
+    public List<Match> retrieveAllMatchesBySportTypeId(int id){
+        query(RETRIEVE_MATCHES_BY_SPORT_TYPE_ID_QUERY, Collections.singletonList(id));
+        return Collections.emptyList();
     }
 
-    public String retrieveSecondTeamById(int id) {
-        return null;
-    }
-
-    public LocalDateTime retrieveStartById(int id) {
-        return null;
-    }
+//    public List<Match> retrieveAllMatchesByFirstTeamId(int id){
+//
+//    }
+//
+//    public List<Match> retrieveAllMatchesBySecondTeamId(int id){
+//
+//    }
 
     // TODO add get all matches by date/sport type
 
