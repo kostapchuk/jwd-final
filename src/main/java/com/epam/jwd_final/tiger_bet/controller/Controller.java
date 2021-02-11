7ad46ea9c9a134cf461cc9c3155f74288313f932
xfase <xfaseex1@gmail.com>
2021-02-11 21:34:53 +0300

    feat: add doPost and processing

--- src/main/java/com/epam/jwd_final/tiger_bet/controller/Controller.java
+++ src/main/java/com/epam/jwd_final/tiger_bet/controller/Controller.java
@@ -1,8 +1,11 @@
 package com.epam.jwd_final.tiger_bet.controller;
 
+import com.epam.jwd_final.tiger_bet.command.Command;
+import com.epam.jwd_final.tiger_bet.command.ResponseContext;
 import com.epam.jwd_final.tiger_bet.dao.UserDAO;
 import com.epam.jwd_final.tiger_bet.domain.User;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,13 +16,29 @@ import java.sql.SQLException;
 
 @WebServlet("/user")
 public class Controller extends HttpServlet {
+
+    private static final String COMMAND_PARAMETER_NAME = "command";
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        process(req, resp);
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try {
-            User userByID = new UserDAO().getUserByID(2);
-            resp.getWriter().println(userByID.toString());
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        process(req, resp);
+    }
+
+    private void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        process(req, resp);
+        final String command = req.getParameter(COMMAND_PARAMETER_NAME);
+        final Command businessCommand = Command.of(command);
+        final ResponseContext result = businessCommand.execute(null);
+        if (result.isRedirect()) {
+            // TODO
+        } else {
+            final RequestDispatcher dispatcher = req.getRequestDispatcher(result.getPage());
+            dispatcher.forward(req, resp);
         }
     }
 }
