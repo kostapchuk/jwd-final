a7d0414e164f6a9ca3e3d3e079c864fbb3256528
xfase <xfaseex1@gmail.com>
2021-02-11 12:38:11 +0300

    feat: add match factory

---
+++ src/main/java/com/epam/jwd_final/tiger_bet/factory/impl/MatchFactory.java
@@ -0,0 +1,22 @@
+package com.epam.jwd_final.tiger_bet.factory.impl;
+
+import com.epam.jwd_final.tiger_bet.domain.Match;
+import com.epam.jwd_final.tiger_bet.domain.Sport;
+import com.epam.jwd_final.tiger_bet.factory.EntityFactory;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class MatchFactory implements EntityFactory<Match> {
+
+    @Override
+    public Match create(Object... params) {
+        return new Match(
+                Integer.parseInt(String.valueOf(params[0])),
+                Sport.FOOTBALL.resolveSportById(Integer.parseInt(String.valueOf(params[1]))),
+                LocalDateTime.parse((String) params[2], DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                (String) params[3],
+                (String) params[4]
+        );
+    }
+}
