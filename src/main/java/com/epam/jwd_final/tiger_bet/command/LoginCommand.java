6ff6806636381d2eaed448766c94124d72a7f65b
xfase <xfaseex1@gmail.com>
2021-02-12 16:37:03 +0300

    feat: add impl to execute method

--- src/main/java/com/epam/jwd_final/tiger_bet/command/LoginCommand.java
+++ src/main/java/com/epam/jwd_final/tiger_bet/command/LoginCommand.java
@@ -2,6 +2,7 @@ package com.epam.jwd_final.tiger_bet.command;
 
 import com.epam.jwd_final.tiger_bet.command.context.RequestContext;
 import com.epam.jwd_final.tiger_bet.command.context.ResponseContext;
+import com.epam.jwd_final.tiger_bet.service.impl.UserService;
 
 public enum LoginCommand implements Command {
 
@@ -9,6 +10,9 @@ public enum LoginCommand implements Command {
 
     @Override
     public ResponseContext execute(RequestContext req) {
-        return null;
+        final String email = req.getParameter("email");
+        final String password = req.getParameter("password");
+
+        return UserService.INSTANCE.logIn(email, password) ? ALL_MATCHES_RESPONSE : new ErrorResponse();
     }
 }
