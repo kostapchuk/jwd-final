75a04a1d880bff89c99ca861f98abcb310d102f7
xfase <xfaseex1@gmail.com>
2021-02-12 21:41:39 +0300

    refactor: remove unnecessary commands

--- src/main/java/com/epam/jwd_final/tiger_bet/command/CommandManager.java
+++ src/main/java/com/epam/jwd_final/tiger_bet/command/CommandManager.java
@@ -1,15 +1,10 @@
 package com.epam.jwd_final.tiger_bet.command;
 
-import com.epam.jwd_final.tiger_bet.command.page.ShowAllBetsPage;
 import com.epam.jwd_final.tiger_bet.command.page.ShowMainPage;
 
 public enum CommandManager {
     LOGIN(LoginCommand.INSTANCE),
     SHOW_MAIN_PAGE(ShowMainPage.INSTANCE),
-    SHOW_ALL_BETS(ShowAllBetsPage.INSTANCE),
-    JOIN(JoinCommand.INSTANCE),
-    SIGN_UP(SignUpCommand.INSTANCE),
-    SHOW_ALL_MATCHES(ShowAllMatchesCommand.INSTANCE),
     DEFAULT(ShowMainPage.INSTANCE);
 
     private final Command command;
