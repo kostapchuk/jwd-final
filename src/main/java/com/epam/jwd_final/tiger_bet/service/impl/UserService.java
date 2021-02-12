8925ea2c05dfa43251ce6ad618fdf680798df265
xfase <xfaseex1@gmail.com>
2021-02-12 21:52:37 +0300

    refactor: delete useless code

--- src/main/java/com/epam/jwd_final/tiger_bet/service/impl/UserService.java
+++ src/main/java/com/epam/jwd_final/tiger_bet/service/impl/UserService.java
@@ -14,20 +14,6 @@ public enum UserService {
 
     private static final UserDAO USER_DAO = new UserDAO();
 
-    public User createUser(String name, String email, String password, BigDecimal balance, Role role) {
-        return new User(name, email, password, balance, role);
-    }
-
-    public boolean saveUser(User user) {
-        return USER_DAO.saveUser(user);
-    }
-
     public boolean logIn(String name, String password) {
-        final Optional<User> optionalUser = userDAO.retrieveUserByEmail(email);
-        boolean passwordEquality = false;
-        if (optionalUser.isPresent()) {
-            passwordEquality = DigestUtils.md5Hex(password).equals(optionalUser.get().getPassword());
-        }
-        return userDAO.retrieveAllEmails().contains(email) && passwordEquality;
     }
 }
