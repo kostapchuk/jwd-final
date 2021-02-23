f699968c57dde8404ae7ea9fcb2fe00efbfa3fdc
xfase <xfaseex1@gmail.com>
2021-02-23 20:48:16 +0300

    refactor: major cleaning up code

---
+++ src/main/java/com/epam/jwd_final/web/command/UpdateBetCommand.java
@@ -0,0 +1,10 @@
+package com.epam.jwd_final.web.command;
+
+import com.epam.jwd_final.web.exception.CommandException;
+
+public class UpdateCommand implements Command {
+    @Override
+    public ResponseContext execute(RequestContext req) throws CommandException {
+
+    }
+}
---
+++ src/main/java/com/epam/jwd_final/web/domain/MultiplierDto.java
@@ -0,0 +1,2 @@
+package com.epam.jwd_final.web.domain;public class MultiplierDto {
+}
